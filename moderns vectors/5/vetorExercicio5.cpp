eada385d838d7accb3f066f1d8cac45d71eca8ab
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-03-15 09:58:36 +0100

    Update vetorExercicio5.cpp

--- moderns vectors/5/vetorExercicio5.cpp
+++ moderns vectors/5/vetorExercicio5.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
 int main(){
 	
-	vector<size_t>nums(10);
-	size_t qtd_par{};
+	vector<int>nums(10);
+	int qtd_values_p{};
 	
 	for(size_t i=0;i < nums.size();i++){
 		
-		std::cout<<"Numero"<<i+1<<"-> ";
-		std::cin>>nums.at(i);
+		cout<<"Numero"<<i+1<<"-> ";
+		cin>>nums.at(i);
 		
 		if(nums.at(i) % 2 == 0){	
-			qtd_par++;
+			qtd_values_p++;
 		}
 	}
-	std::cout<<"Quantidade de numeros pares: "<<qtd_par;
+	std::cout<<"Quantidade de numeros pares: "<<qtd_values_p;
 	
 return 0;	
 }
