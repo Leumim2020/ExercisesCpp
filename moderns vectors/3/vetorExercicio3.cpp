31e1cbd751a3d63dd75281a23142268c6b90e303
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-03-15 10:08:58 +0100

    Update vetorExercicio3.cpp

--- moderns vectors/3/vetorExercicio3.cpp
+++ moderns vectors/3/vetorExercicio3.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <math.h>
 
-using std::vector;
+using namespace std;
 
 int main(){
 	
@@ -10,16 +10,15 @@ int main(){
 	
 	for(size_t j = 0;j < values.size();j++){
 		
-		std::cout<<"N"<<j+1<<"-> ";
-		std::cin>>values.at(j);
+		cout<<"N"<<j+1<<"-> ";
+		cin>>values.at(j);
 		
 		qd_values.at(j) = pow(values.at(j),2);
 		
 		if(j == values.size()-1){
 			for(size_t i=0;i < values.size();i++){
 			
-				std::cout<<"Number "<<values.at(i)<<" ^ 2 = "<<qd_values.at(i)<<"\n";
-							
+				cout<<"Number "<<values.at(i)<<" ^ 2 = "<<qd_values.at(i)<<"\n";				
 			}
 		}
 	}
