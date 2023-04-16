f6a7106dd3663da6ad798c8862b33bfb1e615378
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-04-16 11:24:25 +0100

    Update main.cpp

--- arrays/26/main.cpp
+++ arrays/26/main.cpp
@@ -1,5 +1,5 @@
-/*Faça um programa que calcule o desvio padrão de um vetor v contendo n = 10 números,
-onde m é a media do vetor.*/
+/*FaÃ§a um programa que calcule o desvio padrÃ£o de um vetor v contendo n = 10 nÃºmeros,
+onde m Ã© a media do vetor.*/
 
 #include <iostream>
 #include <math.h>
@@ -7,29 +7,26 @@ onde m é a media do vetor.*/
 
 int main(){
 	
-	double v[size],soma=0,media=0,somatorio=0,desvio_padrao; 
-	//double r,s;
+	double v[size],soma=0,media=0,somatorio=0,desvio_padrao=0;
 	
-	// input valores
-	for(int i=0;i<size;i++){
+	for(int i=0;i < size;i++){
 		std::cout<<"Valor"<<i+1<<"-> ";
 		std::cin>>v[i];
 		soma+=v[i];
 	}
-	// calculando a média do vetor
+	
 	media = soma / size;
 	
-	// calcular o somatório
+	// calcular o somatÃ³rio
 	for(int j=0; j < size; j++){
-		// função pow(x,y) calcula a potência sendo x a base e y o expoente
 		somatorio += pow(v[j] - media,2);
 		/*Ou r = v[i] - media; 
 			somatorio += r*r;
 		*/
 	}
 	
-	// calcular a raiz quadrada do desvio padrão
-	desvio_padrao= sqrt(somatorio/(size-1));
+	
+	desvio_padrao = sqrt(somatorio/(size-1));
 	/*Ou s = somatorio/(size-1);
 		desvio_padrao = sqrt(s);
 	*/
