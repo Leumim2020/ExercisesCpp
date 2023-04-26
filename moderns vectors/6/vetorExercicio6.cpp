fe6e3b92c1d6083b7914d1196b7471fe92de2543
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-04-26 02:01:43 +0100

    Exercise 6 Vector solved
    Make a program that receives from the user a vector with 10 positions. Then you have to
    the largest and smallest element of the vector be printed.

--- moderns vectors/6/vetorExercicio6.cpp
+++ moderns vectors/6/vetorExercicio6.cpp
@@ -1,8 +1,3 @@
-/*
-	Faça um programa que receba do usuário um vetor com 10 posições. Em seguida deverá
-	ser impresso o maior e o menor elemento do vetor.
-*/
-
 #include <iostream>
 #include <vector>
 
