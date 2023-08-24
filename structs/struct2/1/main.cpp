7e0030845c842b493244e96ed3236276e67ffc14
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-08-24 22:01:23 +0100

    Update main.cpp

--- structs/struct2/1/main.cpp
+++ structs/struct2/1/main.cpp
@@ -12,8 +12,6 @@
 		horario horario_comprimesse{};
 		std::string text_comprimesse{""};
 	};
-	
-//C:\Dev-Cpp\TDM-GCC-64\x86_64-w64-mingw32\bin\ld.exe	C:/Dev-Cpp/TDM-GCC-64/x86_64-w64-mingw32/lib32/libmingw32.a(lib32_libmingw32_a-crt0_c.o): in function `main':
 
 int main(){
 	
