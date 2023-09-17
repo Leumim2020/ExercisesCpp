3ebd195df877fe2ce7316eb7f05d0bea19e805aa
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-09-17 21:12:19 +0100

    Update main.cpp

--- structs/struct2/10/main.cpp
+++ structs/struct2/10/main.cpp
@@ -1,24 +1,43 @@
+#include <limits>
 #include <iostream>
 #include <vector>
-const unsigned short  _nw = 51,_addw = 21,_phnw = 10;
+#include <string>
 
-struct PEOPLE{
-	char name[_nw],address[_addw],phoneNumber[_phnw];	
+struct word{
+	std::string people,rua;
+	int age;
 };
 
-int main() {
+int main(){
 	
-	std::vector <PEOPLE> p(5);
-	
-	for(auto i = 0; i < 5 ; ++i){
-		std::cout << "Type name: ";
-		std::cin.getline(p.at(i).name,_nw);
-		std::cout << "Type address: ";
-		std::cin.getline(p.at(i).address,_addw);
-		std::cout << "Type phone number:(+244) ";
-		std::cin.getline(p.at(i).phoneNumber,_phnw);
+	std::vector <word> w1(2);
+
+	for(auto i = 0; i < w1.size(); ++i){
+		std::cout << "Name: ";
+		std::getline(std::cin,w1.at(i).people);
+		std::cout << "Street: ";
+		std::getline(std::cin,w1.at(i).rua);
+		std::cout << "Age: ";
+		std::cin >> w1.at(i).age;
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+
 	}
 	
+	w1.at(0) = w1.at(1);
+	
+	/*for(auto i = 0; i < w1.size(); ++i){
+		for(auto j = 0; j < w1.size(); ++j){
+			if(w1.at(i).people.at(0) < w1.at(j).people.at(0)){
+				std::string change = w1.at(i).people;
+				w1.at(i).people = w1.at(j).people;
+				w1.at(j).people = change;
+			}
+		}
+	}
+	*/
+	std:: cout << "----------------------------------\n";
+	for(auto i = 0; i < w1.size(); ++i)
+		std::cout << w1.at(i).people << " " << w1.at(i).rua << " " << w1.at(i).age << " " <<std::endl;
 	
 	
 	return 0;
