63479b328533cedfde36ba2e89c9d98adf59a4ef
Leumim Da Rocha Camuenga <71234060+Leumim2020@users.noreply.github.com>
2023-10-13 22:07:05 +0100

    Add files via upload

---
+++ structs/struct2/14/exerciseStruct14.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <limits>
+#include <vector>
+#include "validatepriceofcar.cpp"
+
+void searchcheapcar(std::vector <CAR> *scar);
+
+int main(int argc,char *argv[]){
+
+	std::vector <CAR> vcar(5);	
+	
+	for(auto i = 0; i < vcar.size(); ++i){
+		std::cout << "Type Mark of the Car: ";
+		std::cin.getline(vcar.at(i).mark,_markW);
+		std::cout << "Type Year of the Car: ";
+		std::cin >> vcar.at(i).year;
+		std::cout << "Type Price$ of the Car: ";
+		std::cin >> vcar.at(i).price;
+		std::cout << "---------------------------------" << std::endl;
+
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+	}
+	
+	std::cout << std::endl;
+	searchcheapcar(&vcar);
+	
+	return 0;
+}
+
---
+++ structs/struct2/14/validatepriceofcar.cpp
@@ -0,0 +1,37 @@
+#define _markW 16;
+
+struct CAR{
+	char mark[_markW];
+	unsigned short year{};
+	double price{};
+};
+
+void searchcheapcar(std::vector <CAR> *scar){
+	
+	auto j = 0;
+	double dprice{};
+	
+	while(true){
+		
+		std::cout << "Search cheaps Cars type price$: ";
+		std::cin >> dprice;
+		
+		if(dprice == 0)
+			break;
+		else{
+			std::cout << "---------------------------------" << std::endl;
+			while(j < scar->size()){
+				if(dprice > scar->at(j).price){
+					std::cout << std::endl 
+			          << "Mark of the Car:" << scar->at(j).mark << std::endl 
+					  << "Year of the Car: " << scar->at(j).year << std::endl
+					  << "Price$ of the Car: " << scar->at(j).price;
+				}
+			++j;
+			}
+			std::cout << std::endl 
+					  << "---------------------------------" << std::endl;
+		}
+	}
+		
+}
